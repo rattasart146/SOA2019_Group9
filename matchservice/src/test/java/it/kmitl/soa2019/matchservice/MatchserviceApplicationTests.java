5518f2f5f350678950deede8afd63085de245a95
imearth <38610522+imearth@users.noreply.github.com>
2019-03-18 22:31:51 +0700

    เพิ่ม matchservice ใหม่

---
+++ matchservice/src/test/java/it/kmitl/soa2019/matchservice/MatchserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package it.kmitl.soa2019.matchservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatchserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
