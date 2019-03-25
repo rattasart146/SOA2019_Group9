f265a2ba4a5f68f3a3d9e48b0d90bd37e4df9a93
imearth <38610522+imearth@users.noreply.github.com>
2019-03-25 20:29:05 +0700

    เพิ่ม Test

--- matchservice/src/test/java/it/kmitl/soa2019/matchservice/match_service/matchServiceTest.java
+++ matchservice/src/test/java/it/kmitl/soa2019/matchservice/match_service/matchServiceTest.java
@@ -6,32 +6,39 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MockitoJUnitRunner.class)
 public class matchServiceTest {
 
-    @Mock
-    matchRepository matchRepository;
+    private ArrayList<Match> matchList = new ArrayList<>(Arrays.asList(
+            new Match("My game", "1", "somchai", "ladkrabang", "Registering"),
+            new Match("Big game", "2", "somrak", "bangkok", "During Match"),
+            new Match("Bang-ra-chan game", "3", "tongmhen", "newyork", "Registering"),
+            new Match("SEA game", "4", "nipon", "pattaya", "Registering")
+    ));
+
+    private ArrayList<Match> matchListExpected = new ArrayList<>(Arrays.asList(
+            new Match("My game", "1", "somchai", "ladkrabang", "Registering")
+            ));
+
+
 
     @Test
-    public void searchbyid (){
-        Match matchstub = new Match();
-        matchstub.setId("1");
-        matchstub.setLocation("Somewhere");
-        matchstub.setMatchname("My game");
-        matchstub.setOwner_username("alex");
-        matchstub.setStatus("Openning");
+    public void getMatch() {
+        List<Match> matchesIdentityById = new ArrayList<>();
+        for(int count = 0;count < matchList.size();count++) {
+            if(matchList.get(count).getId().equals("1")) {
+                matchesIdentityById.add(matchList.get(count));
+            }
+        }
 
-
-        /*
-                     @Mock
-                matchRepository matchRepository;
-
-                @Test
-                public String searchbyid (int id){
-                    Optional<Match> SomeMatch = matchRepository.findById(id);
-                    return "Hello "+ SomeMatch.get().getId();
-                }
-         */
-
+        assertEquals(matchListExpected.size(), matchesIdentityById.size());
+        assertEquals(matchListExpected.get(0).getId(), matchesIdentityById.get(0).getId());
+        assertEquals(matchListExpected.get(0).getId(), matchesIdentityById.get(0).getId());
     }
 }
