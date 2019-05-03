1b135a8b23db1b693ed1c7fa1e045d504a14a3c1
imearth <38610522+imearth@users.noreply.github.com>
2019-05-03 16:01:17 +0700

    อัพใหม่

---
+++ backend/userservice/src/service/UserService.js
@@ -0,0 +1,134 @@
+const mockUser = require('../../data/mockUser')
+const _ = require('underscore')
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+
+
+const HOST_MYSQL = '35.240.225.238'
+const PORT_MYSQL = '3308'
+// connection configurations
+var con = mysql.createConnection({
+    host: HOST_MYSQL,
+    port: PORT_MYSQL,
+    user: 'root',
+    password: 'root',
+    database: 'user_database'
+});
+// connect to database
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+exports.getUserByUserId = (req, res) => {
+    /*
+    var filterUser = _.where(mockUser, {userid: parseInt(req.params.userid)})
+
+    if(filterUser == "") {
+        return res.sendStatus(404)
+    } else {
+        var docs = []
+        
+        for(var count in filterUser) {
+            docs.push({userid: filterUser[count].userid,
+                username: filterUser[count].username,
+                firstname: filterUser[count].firstname,
+                lastname: filterUser[count].lastname,
+            })
+        }
+        return res.status(200).json(docs)
+    }
+    */
+
+
+
+
+    let user_id = req.params.userid;
+    if (!user_id) {
+        return res.status(400).send({
+            error: true,
+            message: 'Please provide user_id'
+        });
+    }
+    con.query('SELECT * FROM user where user_id=?', user_id, function(error, results, fields) {
+        if (error) throw error;
+        return res.send({
+            error: false,
+            data: results[0],
+            message: 'users list.'
+        });
+    });
+}
+exports.getAllUser = (req, res) => {
+    /*
+    if (mockUser == "") {
+        return res.sendStatus(404);
+    } else {
+        return res.status(200).json(mockUser);
+    }
+    */
+    con.query("SELECT * FROM user", function(error, results, fields) {
+        if (results.length) {
+            console.log("GET USER IS OK")
+            return res.status(200).json(results)
+        } else {
+            return res.status(404)
+        }
+    });
+}
+exports.postNewUser = (req, res) => {
+    /*
+    var docs = req.body
+    mockUser.push(docs)
+    return res.status(201).send({isCreateNewUser:true})
+	*/
+    let username = req.body.username;
+    let password = req.body.password;
+    let firstname = req.body.username;
+    let lastname = req.body.lastname;
+    if (!username) {
+        return res.status(400).send({
+            error: true,
+            message: 'Please provide username'
+        });
+    }
+    if (!password) {
+        return res.status(400).send({
+            error: true,
+            message: 'Please provide password'
+        });
+    }
+    if (!firstname) {
+        return res.status(400).send({
+            error: true,
+            message: 'Please provide firstname'
+        });
+    }
+    if (!lastname) {
+        return res.status(400).send({
+            error: true,
+            message: 'Please provide lastname'
+        });
+    }
+    con.query("SELECT username from user where username = ?",username , function(error, results, fields) {
+    	console.log(results)
+        if (results.length) {
+        	return res.send("deplicate username");   
+        }
+
+        else{
+        	con.query("INSERT INTO user SET ? ", {
+                username: username,
+                password: password,
+                firstname: firstname,
+                lastname: lastname
+            }, function(error, results, fields) {
+                if (error) throw error;
+                return res.send({
+                    error: false,
+                    data: results,
+                    message: 'New user has been created successfully.'
+                });
+            });
+        }
+    });
+}
